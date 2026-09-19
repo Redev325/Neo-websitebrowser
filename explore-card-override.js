33e32539a942790c37c71c29331306745480c0f9
Redev325 <s1991375@online.houstonisd.org>
2026-09-19 17:34:39 -0500

    Replace Explore Placeholder 2 title

--- explore-card-override.js
+++ explore-card-override.js
@@ -4,6 +4,7 @@
   window.__neoSimpleGameUIV5 = true;
 
   var FIRST_TITLE = 'VS IMPOSTOR: LEGACY';
+  var SECOND_TITLE = 'Vs sonic.exe 2.0-4.0';
   var VIEWER_TITLE = 'VS Impostor:Legacy';
   var FIRST_IMAGE = 'https://camo.githubusercontent.com/831fc627f5c4f8e44b50a16d9eaf4220feacc947f960c04febb3779e36a30056/68747470733a2f2f66696c65732e67616d6562616e616e612e636f6d2f696d672f73732f6d6f64732f363965636665623236386565632e6a7067';
   var ICON_URL = 'https://plain-enam-prod-public.komododecks.com/202609/17/Ap8nvejCSQjcy3kMXAbE/image.png';
@@ -67,6 +68,7 @@
       var svgs = preview.querySelectorAll('svg');
       for (var s = 0; s < svgs.length; s++) svgs[s].style.display = 'none';
     }
+    if (index === 1) title.textContent = SECOND_TITLE;
     title.style.display = 'block';
     title.style.width = '100%';
     title.style.boxSizing = 'border-box';
