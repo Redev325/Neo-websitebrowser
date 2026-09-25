4d59c09934ef57eccd4e393c82c07b92228cc76c
Redev325 <s1991375@online.houstonisd.org>
2026-09-24 22:37:11 -0500

    Fix proxy HTML rewriting crash

--- api/proxy.js
+++ api/proxy.js
@@ -377,10 +377,12 @@ function buildBridge(proxyOrigin, pageBase) {
 }
 
 function rewriteLinks(html, base, proxyOrigin) {
-  let snokidoPage = /^(?:www\.)?snokido\.(?:com|fr)$/i.test(bu.hostname) && /^\/game(?:\/|$)/i.test(bu.pathname);
+  let snokidoPage = false;
   try {
     const bu = new URL(base);
-    snokidoPage = /^(?:www\.)?snokido\.(?:com|fr)$/i.test(bu.hostname) && /^\/game(?:\/|$)/i.test(bu.pathname);
+    snokidoPage =
+      /^(?:www\.)?snokido\.(?:com|fr)$/i.test(bu.hostname) &&
+      /^\/game(?:\/|$)/i.test(bu.pathname);
   } catch {}
 
   const prox = (u) => proxyOrigin + "/api/proxy?url=" + encodeURIComponent(u.toString());
