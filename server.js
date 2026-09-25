be2f02d7078b824c5343ed66acf5c56591acd7a8
Redev325 <s1991375@online.houstonisd.org>
2026-09-24 23:37:48 -0500

    Allow larger proxied request bodies

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const searchHandler = require("./api/search.js");
 
 const app = express();
 app.disable("x-powered-by");
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "32mb" }));
 
 app.all("/api/proxy", (req, res) => {
   try {
